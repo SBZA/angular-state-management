308ca6ae7e189eb9811187edca16d2f6ae133b8d
Sontaga <sonti96@live.co.uk>
2020-06-23 15:11:59 +0200

    Added EmployeeReducer
    All state changes are happening inside the reducer based on the selected ‘Action.

---
+++ angular-state-management/src/app/reducers/employee.reducer.ts
@@ -0,0 +1,20 @@
+import {ActionEx, EmployeeActionsType} from 'src/app/actions/employee.actions';
+import { stat } from 'fs';
+
+export const initialState = [];
+
+export function EmployeeReducer(state = initialState, action: ActionEx){
+  switch(action.type) {
+    case EmployeeActionsType.Add:
+      return [...state, action.payload];
+
+      case EmployeeActionsType.Remove:
+        return [
+          ...state.slice(0, action.payload),
+          ...state.slice(action.payload + 1)
+        ];
+
+        default:
+          return state;
+  }
+}
